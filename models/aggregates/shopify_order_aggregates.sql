d777a96fd0e1ea7aa0096909c9ae87815c02e907
chris-schmid <chris.schmid@lacolombe.net>
2021-12-06 10:51:06 -0500

    Moved all discount calculations to item-level, using orders__line_items__discount_allocations table

--- models/aggregates/shopify_order_aggregates.sql
+++ models/aggregates/shopify_order_aggregates.sql
@@ -6,6 +6,7 @@ select
 	sum(quantity) as count_of_items,
 	sum(line_item_net_sales) as net_sales,
 	sum(line_item_gross_sales) as gross_sales,
+	sum(line_item_discount) as order_discount,
 	sum(line_item_weight) as weight,
 	max(updated_at) as updated_at
 
